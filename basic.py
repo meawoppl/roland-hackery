9e9712dc1f81ec803404bc3ccea456fd44ab235e
meawoppl <meawoppl@gmail.com>
2020-09-14 15:36:39 -0700

    WIP

--- basic.py
+++ basic.py
@@ -1,205 +1,143 @@
-import cups
-import tempfile
-import os
-import time
-import subprocess
-import pprint
-
-
-
-def fmt(n: float) -> str:
-    return str(round(n, 2))
-
-
-UNITS_PER_MM = 100
-
-def cvt(val: float):
-    return int(round(val * UNITS_PER_MM, 0))
-
-
-class RMDCommander:
-    def __init__(self, taskName=""):
-        self.taskName = taskName
-        self.script = ""
-        self._append_raw('^DF')
-        self._append_raw('!MC0')
-
-    def _append_raw(self, string: str):
-        print('Adding Command: ' + string)
-        self.script += string + ';\r\n'
-
-    def _append_format(self, string: str, *args):
-        self._append_raw(string.format(*args))
-
-    def speed(self, v: float):
-        """
-        Set the maximum speed of 2d and 3d moves in mm/second
-        """
-        self._append_format('F{:.1}', v)
-        self._append_format('V{:.1}', v)
-        self._append_format('VS{:.1}', v)
-        
-    def dwell(self, timeMs: float):
-        assert timeMs > 0 and timeMs < 32767, timeMs
-        self._append_format('W{:.0f}', int(timeMs))
-
-    def absXY(self, x: float, y: float):
-        self._append_format("PA {}, {}", cvt(x), cvt(y))
-
-    def absXYZ(self, x: float, y: float):
-        self._append_raw('PA')
-        self._append_format('Z{},{},{}', cvt(x), cvt(y), cvt(z))
-
-    def relXY(self, x: float, y:float):
-        self.relXYZ(x, y, 0.0)
-
-    def relXYZ(self, x: float=0, y: float=0, z: float=0):
-        self._append_raw('PR')
-        self._append_format('Z{},{},{}', cvt(x), cvt(y), cvt(z))
-
-    def spindleOn(self, speed: float):
-        assert speed > 100 and speed < 10000, speed
-        self._append_format("!RC {:.0f}", speed)
-        self._append_raw("!MC1")
-
-    def spindleOff(self):
-        self._append_raw("!MC0")
-
-
-    def getOutput(self):
-        return self.script
-
-
-
-class RolandMill:
-    def __init__(self):
-        self.conn = cups.Connection()
-
-        print('Searching for a roland mill')
-        for pKey, pVal in self.conn.getPrinters().items():
-            if (pKey.startswith('Roland')):
-                print("Found: " + pKey)
-                self.name = pKey
-                self.prop = pVal
-                self.cancelPending()
-                return
-        raise RuntimeError('No Roland Devices Found in '.format(self.conn.getPrinters().keys()) )
-
-    def cancelPending(self):
-        self.conn.cancelAllJobs(uri=self.prop['device-uri'])
-
-
-    def _printText(self, text: str, taskName: str):
-        with tempfile.NamedTemporaryFile('w') as f:
-            f.write(text)
-            f.flush()
-            return self._printFile(f.name, taskName)
-
-    def _printFile(self, filename: str, taskName: str):
-        assert os.path.exists(filename)
-        return self.conn.printFile(self.name, filename, taskName, {})
-
-    def runCommand(self, cmd: RMDCommander):
-        return self._printText(cmd.getOutput(), cmd.taskName)
-
-    def waitUntilDone(self):
-        cmd = RMDCommander('WaitTask')
-        cmd.dwell(10)
-        waitJobID = self.runCommand(cmd)
-        done = False
-        while not done:
-            results = self.conn.getJobAttributes(waitJobID)
-            done = results['time-at-completed'] != None
-            print("Waiting on job finish")
-            pprint.pprint(results)
-            if not done:
-                time.sleep(1)
-
+from pyroland.cmd import RMDCommander
+from pyroland.mill import RolandMill
 
 
 import numpy as np
 
-def xyPlane(xyMin: tuple, xyMax: tuple, toolSize: float):
-    # Determine the number of passes
-    # Compute the x starting points
-    # move to 
-    # ^Y move to x
 
-    halfTool = toolSize / 2
-    xMin, yMin = (v + halfTool for v in xyMin) 
-    xMax, yMax = (v - halfTool for v in xyMax) 
+class EnhancedCommand(RMDCommander):
+    def fillInside(self, xyMin: tuple, xyMax: tuple, toolSize: float, zHeight: float):
+        halfTool = toolSize / 2
+        xyMin = (v + halfTool for v in xyMin) 
+        xyMax = (v - halfTool for v in xyMax) 
 
-    dx = xMax - xMin
-    dy = yMax - yMin
+        # Box out the move to avoid leaving tails later
+        self.fillRect(xyMin, xyMax, toolSize, zHeight)
 
-    cmd = RMDCommander()
-    cmd.speed(2.0)
+    def fillRect(self, xyMin: tuple, xyMax: tuple, toolSize: float, zHeight: float):
+        # Determine the number of passes
+        # Compute the x starting points
+        # move to 
+        # ^Y move to x
 
-    # Box out the move to avoid leaving tails later
-    cmd.absXY(xMin, yMin)
-    cmd.absXY(xMax, yMin)    
-    cmd.absXY(xMax, yMax)
-    cmd.absXY(xMin, yMax)
-    cmd.absXY(xMin, yMin)
+        xMin, yMin = xyMin 
+        xMax, yMax = xyMax
 
-    # Compute the line offsets to work in
-    passes = int(np.ceil(dx / toolSize))
-    offsets = np.linspace(xyMin[0], xyMax[0], passes)
-    for n, xOffset in enumerate(offsets):
-        if n % 2 == 0:
-            cmd.absXY(xOffset, yMin)
-            cmd.relXY(0.0, dy)
-        else:
-            cmd.absXY(xOffset, yMax)
-            cmd.relXY(0.0, -dy)
+        dx = xMax - xMin
+        dy = yMax - yMin
+        
+        # Box out the move to avoid leaving tails later
+        self.strokeRect(xyMin, xyMax, zHeight)
 
-    return cmd
+        # Compute the line offsets to work in
+        passes = int(np.ceil(dx / (toolSize*0.9)))
+        offsets = np.linspace(xMin, xMax, passes)
+        for n, xOffset in enumerate(offsets):
+            if n % 2 == 0:
+                self.absXYZ(xOffset, yMin, zHeight)
+                self.absXYZ(xOffset, yMax, zHeight)
+            else:
+                self.absXYZ(xOffset, yMax, zHeight)
+                self.absXYZ(xOffset, yMin, zHeight)
+
+    def strokeRect(self, xyMin: tuple, xyMax: tuple, zHeight: float):
+        xMin, yMin = xyMin 
+        xMax, yMax = xyMax 
+
+        self.absXY(xMin, yMin)
+        self.absXYZ(xMin, yMin, zHeight)    
+        self.absXYZ(xMax, yMin, zHeight)    
+        self.absXYZ(xMax, yMax, zHeight)
+        self.absXYZ(xMin, yMax, zHeight)
+        self.absXYZ(xMin, yMin, zHeight)
+
+    def strokeInside(self, xyMin: tuple, xyMax: tuple, toolSize: float, zHeight: float):
+        # Determine the number of passes
+        # Compute the x starting points
+        # move to 
+        # ^Y move to x
+
+        halfTool = toolSize / 2
+        xyMin = (v + halfTool for v in xyMin) 
+        xyMax = (v - halfTool for v in xyMax) 
+
+        # Box out the move to avoid leaving tails later
+        self.strokeRect(xyMin, xyMax, zHeight)
+
+
 
 mill = RolandMill()
-cmd = xyPlane((-30, -30), (30, 30), 9.5)
-mill.runCommand(cmd)
-mill.waitUntilDone()
 
-# mill = RolandMill()
-# cmd = RMDCommander()
-# cmd.absXY(-100, -100)
+def cutting_board_outline():
+    edge_min = (-190, -290)
+    edge_max = (151, 105)
+    bit_size = 9.5 - 1.63
+    cmd = EnhancedCommand()
+    with cmd.cutting_context(10000, 40):
+        for z in np.linspace(-58, -86, 30):
+            cmd.strokeInside(edge_min, edge_max, bit_size, z)
+        cmd.liftZ()
 
-# mill.runCommand(cmd)
-# mill.waitUntilDone()
-
-
-def fun():
-    d = 500
-
-    for v in range(2, 5):
-        cmd.speed(v)
-        cmd.relXYZ(d, 0, 0)
-        cmd.relXYZ(-d, 0, 0)
-        cmd.relXYZ(0, +d, 0)
-        cmd.relXYZ(0, -d, 0)
-        cmd.relXYZ(0, 0, +d)
-        cmd.relXYZ(0, 0, -d)
-import math
-
-
-
-def circle_test():
-    r = 10
-    x0 = r
-    y0 = 0
-
-    cmd = RMDCommander()
-    cmd.spindleOn(2000)
-    cmd.speed(0.5)
-
-    for t in np.linspace(0, 2 * np.pi, 93):
-        dx = r * np.cos(t)
-        dy = r * np.sin(t)  
-        cmd.relXY(x0 - dx, y0 - dy)
-        x0 = dx
-        y0 = dy
-
-    cmd.spindleOff()
-
-    job = mill.runCommand(cmd)
+    mill.runCommand(cmd)
     mill.waitUntilDone()
+
+# Rounded inlay
+def inlay():
+    edge_min = (-170, -270)
+    edge_max = (131, 85)
+    bit_size = 9.5 - 1.63
+    z = -56
+    cmd = EnhancedCommand()
+    with cmd.cutting_context(10000, 40):
+        cmd.strokeInside(edge_min, edge_max, bit_size, z)
+        cmd.liftZ()
+
+    mill.runCommand(cmd)
+    mill.waitUntilDone()
+
+
+def face():
+    bit_size = 9.5
+
+    edge_min = (-200.895, -290.585)
+    edge_max = (130.105, 95.415)
+
+    cmd = EnhancedCommand()
+    cmd.liftZ()
+    with cmd.cutting_context(10000, 20):
+        for zHeight in [-44.5]:
+            cmd.fillRect(edge_min, edge_max, bit_size, zHeight)
+    
+    mill.runCommand(cmd)
+
+
+def pockets():
+    bit_size = 9.5
+
+    edge_min = (-205.895, -295.585)
+    edge_max = (135.105, 99.415)
+
+    edge_min = tuple(v + 30 for v in edge_min)
+    edge_max = tuple(v - 30 for v in edge_max)
+
+    x1, x2, x3, x4 = np.linspace(edge_min[0], edge_max[0], 4)
+    y1, y2, y3, y4 = np.linspace(edge_min[1], edge_max[1], 4)
+
+    cmd = EnhancedCommand()
+    with cmd.cutting_context(10000, 40):
+        for z in np.linspace(-50, -75, 10):
+            cmd.liftZ()
+            cmd.fillRect((x1, y1), (x4, y2), bit_size, z)
+
+            cmd.liftZ()
+            cmd.fillRect((x1, y3), (x4, y4), bit_size, z)
+
+            cmd.liftZ()
+            cmd.fillRect((x1, y2-5), (x2, y3+5), bit_size, z)
+
+            cmd.liftZ()
+            cmd.fillRect((x3, y2-5), (x4, y3+5), bit_size, z)
+
+    mill.runCommand(cmd)
+
+pockets()
